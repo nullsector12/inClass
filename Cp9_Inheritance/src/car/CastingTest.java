eccb6198277edb6ba2e813272ed75a83d2cac7f1
KimSeungYeon <rlamd0412@gmail.com>
2020-04-23 18:47:17 +0900

    싱글톤 추가설명 및 오버라이딩연습

--- Cp9_Inheritance/src/car/CastingTest.java
+++ Cp9_Inheritance/src/car/CastingTest.java
@@ -8,12 +8,11 @@ public class CastingTest {
 		FireEngine fire2 = null;
 		Ambulance am = null;
 		
-		
 		fire.water();
+		
 		// 자손클래스 => 조상클래스 형 변환은 생략가능
 		car = fire;
 		
-
 		// 형 변환 자체가 안될수도 있는 경우가 있어서 자동생략 x
 		// 조상클래스 => 자손클래스 형 변환은 명시해줘야함
 		fire2 = (FireEngine)car;
--- Cp9_Inheritance/src/ex/OverRidingTest.java
+++ Cp9_Inheritance/src/ex/OverRidingTest.java
@@ -5,6 +5,7 @@ public class OverRidingTest {
 	public static void main(String[] args) {
 		//  다형성 : 상위클래스 타입의 참조변수에 
 		// 하위클래스의 인스턴스를 참조하는 것.
+		
 		Speaker sp = new BaseEnSpeaker();
 		// 참조변수 sp는 BaseEnSpeaker 인스턴스의 멤버중
 		// Speaker가 가진 멤버만 사용 가능.
@@ -16,7 +17,7 @@ public class OverRidingTest {
 		bSpeaker.setVolume(10);
 		bSpeaker.setBaseRate(20);
 		bSpeaker.showCurrentState();
-
+		System.out.println(bSpeaker.toString());
 	}
 
 }
