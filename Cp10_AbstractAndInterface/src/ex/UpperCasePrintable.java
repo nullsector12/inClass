a3a1b5b654d6b33b0d50d22ecb4c6f2272dc55a1
KimSeungYeon <rlamd0412@gmail.com>
2020-04-28 12:12:14 +0900

    프로젝트명 변경

---
+++ Cp10_AbstractAndInterface/src/ex/UpperCasePrintable.java
@@ -0,0 +1,12 @@
+package ex;
+
+/**
+ * 작성자 : 김승연
+ * 작성 일시 : 2020. 04. 28
+ * ~able 은 다형성을 위해 마킹의 의미를 가지는 인터페이스
+ *  형변환 가능 확인( instanceof )을 위해서만 사용
+ * 
+ */
+public interface UpperCasePrintable {
+
+}
